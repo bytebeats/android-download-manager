32fab0ad9ddf19fd303d1a43a1ed7ca530f6dec4
Peter Pan <happychinapc@gmail.com>
2014-07-22 09:31:14 +0800

    Create LogUtils.java

---
+++ src/com/littlepanpc/downloader/util/LogUtils.java
@@ -0,0 +1,88 @@
+/*
+ * 文件名：LogUtil.java
+ * 版权：<版权>
+ * 描述：<描述>
+ * 创建人：xiaoying
+ * 创建时间：2013-5-16
+ * 修改人：xiaoying
+ * 修改时间：2013-5-16
+ * 版本：v1.0
+ */
+
+package com.littlepanpc.downloader.util;
+
+import android.util.Log;
+
+/**
+ * @class: LogUtils
+ * @Description: to print logging
+ * @author: Peter Pan
+ * @email: happychinapc@gmail.com
+ * @date: 2014-5-19 下午6:01:11
+ * @since: 1.0.0
+ *
+ */
+public class LogUtils {
+
+	private final static boolean PRINTABLE = true;
+	
+	public static void i(String tag, String msg) {
+		if(PRINTABLE) {
+			Log.i(tag, msg);
+		}
+	}
+	
+	public static void i(String tag, Object msg) {
+		if(PRINTABLE) {
+			Log.i(tag, msg.toString());
+		}
+	}
+	
+	public static void w(String tag, String msg) {
+		if(PRINTABLE) {
+			Log.w(tag, msg);
+		}
+	}
+
+	public static void w(String tag, Object msg) {
+		if(PRINTABLE) {
+			Log.w(tag, msg.toString());
+		}
+	}
+	
+	public static void e(String tag, String msg) {
+		if(PRINTABLE) {
+			Log.e(tag, msg);
+		}
+	}
+
+	public static void e(String tag, Object msg) {
+		if(PRINTABLE) {
+			Log.e(tag, msg.toString());
+		}
+	}
+	
+	public static void d(String tag, String msg) {
+		if(PRINTABLE) {
+			Log.d(tag, msg);
+		}
+	}
+	
+	public static void d(String tag, Object msg) {
+		if(PRINTABLE) {
+			Log.d(tag, msg.toString());
+		}
+	}
+	
+	public static void v(String tag, String msg) {
+		if(PRINTABLE) {
+			Log.v(tag, msg);
+		}
+	}
+	
+	public static void v(String tag, Object msg) {
+		if(PRINTABLE) {
+			Log.v(tag, msg.toString());
+		}
+	}
+}
