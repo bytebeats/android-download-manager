749c7b7d85eecc64c36149eadc160c8c66cb7d86
Peter Pan <happychinapc@gmail.com>
2014-07-25 10:22:29 +0800

    Update DownloadingStatusCode.java

--- src/com/littlepanpc/downloader/util/DownloadingStatusCode.java
+++ src/com/littlepanpc/downloader/util/DownloadingStatusCode.java
@@ -1,5 +1,14 @@
 package com.littlepanpc.downloader.util;
 
+/**
+ * @class: DownloadingStatusCode
+ * @Description: downloading error code. Show the reason of bad downloading.
+ * @author: Peter Pan
+ * @email: happychinapc@gmail.com
+ * @date: 2014-7-25 上午10:19:57
+ * @since: 1.0.0
+ *
+ */
 public class DownloadingStatusCode {
 	public static final String ERROR_FILE_EXIST = "100";
 	/** bad URL */
