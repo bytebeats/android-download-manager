1e91fb1409161b4d1f45e3c73b94b33a356d148b
Peter Pan <happychinapc@gmail.com>
2014-07-28 09:06:45 +0800

    Create DownloadingException.java

---
+++ src/com/littlepanpc/downloader/exception/DownloadingException.java
@@ -0,0 +1,20 @@
+package com.littlepanpc.downloader.exception;
+
+public class DownloadingException extends RuntimeException {
+
+	/**
+	 * @Field long serialVersionUID: TODO
+	 */
+	private static final long serialVersionUID = 7449248441719450018L;
+
+	public DownloadingException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DownloadingException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+}
