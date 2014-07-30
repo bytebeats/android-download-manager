76b22a5e275ab6a5b752afff191410ca9a9669b0
Peter Pan <happychinapc@gmail.com>
2014-07-30 10:34:34 +0800

    Update StorageUtils.java

--- src/com/littlepanpc/downloader/util/StorageUtils.java
+++ src/com/littlepanpc/downloader/util/StorageUtils.java
@@ -8,7 +8,12 @@ public class StorageUtils {
 			.getExternalStorageDirectory() + "/downloads";
 	public static final String DOWNLOAD_DIRECTORY_TEMP = Environment
 			.getExternalStorageDirectory() + "/downloads/tmps";
-
+	/**
+	 * @Title: getAvailableStorage
+	 * @Description: obtain available storage
+	 * @return long
+	 * @throws 
+	 */
 	public static long getAvailableStorage() {
 
 		String storageDirectory = Environment.getExternalStorageDirectory()
